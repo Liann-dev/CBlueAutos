78678d3f078c17bcdc5bb9caf5f9967c079e5df6
Rayghj <rayid1805@gmail.com>
2026-04-03 10:26:34 +0700

    Berubah pikiran, jadikan satu file untuk fungsi register dan login menjadi auth.cpp

--- app/auths/auth.cpp
+++ app/auths/auth.cpp
@@ -91,3 +91,38 @@ void registerUser() {
 
 	cout << "Register berhasil. Silakan login.\n";
 }
+
+bool loginUser(User &userTerdaftar) {
+	string username, password;
+
+	cout << "\n>>> LOGIN <<<\n";
+	cout << "Masukkan username: ";
+	getline(cin, username);
+	cout << "Masukkan password: ";
+	getline(cin, password);
+
+	ifstream file(dbUser.c_str());
+
+	for (string line; getline(file, line); ) {
+		stringstream ss(line);
+		string id, fileUsername, filePassword, fileRole;
+
+		getline(ss, id, ',');
+		getline(ss, fileUsername, ',');
+		getline(ss, filePassword, ',');
+		getline(ss, fileRole, ',');
+
+		if (fileUsername == username && filePassword == password) {
+			userTerdaftar.id = stoi(id);
+			userTerdaftar.username = fileUsername;
+			userTerdaftar.password = filePassword;
+			userTerdaftar.role = fileRole;
+
+			file.close();
+			return true;
+		}
+	}
+
+	file.close();
+	return false;
+}
