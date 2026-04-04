d4bac0b814fab2540c47f04fdf632de74a9c3e1f
Berlianda M Putra <berliandamputra@gmail.com>
2026-04-04 17:56:01 +0700

    pembaruan animasi login/register

--- app/main.cpp
+++ app/main.cpp
@@ -3,14 +3,24 @@
 
 using namespace std;
 
-int main() {
+void efekNgetik(string teks, int jedaMs = 30);
+void tampilkanLoading(string pesan = "Memuat", int iterasi = 3);
 
+int main() {
     User userTerdaftar;
     int mulai = 1;
 
+    cout << "\n";
+    tampilkanLoading("Menyalakan mesin C-BlueAutos...", 4);
+    efekNgetik("Memuat database kendaraan...", 40);
+    efekNgetik("Sistem Siap Digunakan!", 60);
+    cout << "===========================\n";
+
     for (; mulai == 1; ) {
         int pilihan;
-        cout << "\n=== C-BlueAutos ===\n";
+        
+        // Menggunakan efek ngetik agar judul menu lebih menarik
+        efekNgetik("\n=== C-BlueAutos ===", 10);
         cout << "1. Login\n";
         cout << "2. Register\n";
         cout << "3. Keluar\n";
@@ -28,16 +38,18 @@ int main() {
             bool loginBerhasil = loginUser(userTerdaftar);
 
             if (loginBerhasil) {
+                tampilkanLoading("Memverifikasi kredensial...", 3);
                 cout << "Login berhasil.\n";
                 cout << "Login sebagai: " << userTerdaftar.role << "\n";
+
             } else {
-                cout << "Kembali ke menu utama.\n";
+                cout << "Kembali ke menu utama.\n"; 
             }
         } else if (pilihan == 2) {
             registerUser();
         } else if (pilihan == 3) {
-                cout << "Program selesai.\n";
-                mulai = 0;
+            efekNgetik("Menutup program... Sampai jumpa!", 40);
+            mulai = 0;
         } else {
             cout << "Pilihan tidak valid.\n";
         }
