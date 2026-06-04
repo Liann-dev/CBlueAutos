d0fdbb644f803ad849f9c2a579d38cf9388e2db1
rifafachrir <rifafachriramadhan1806@gmail.com>
2026-06-04 19:23:26 +0700

    rekomendasi belum beres

---
+++ app/rekomendasiUser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "features.h"
+#include "homeUser.h"
+
+using namespace std;
+
+const string dbMobil = "database_mobil.csv";
+const string dbPrefensi = "database_prefensi.csv";
